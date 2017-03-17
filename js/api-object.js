f695503338ca7ab0830c5a6c97293bdf1f397891
Nicholas Mead <nixter11@uw.edu>
2017-03-17 15:01:16 -0700

    add readme

--- js/api-object.js
+++ js/api-object.js
@@ -29,12 +29,13 @@ DoctorSearch.prototype.getDoctors = function(coords) {
     for (i = 0; i < coords.length; i++) {
         console.log(coords[i]);
     }
-    var requestUrl = "https://api.betterdoctor.com/2016-03-01/doctors?query=" + coords[2] + "&location=" + coords[0] + "%2C" + coords[1] + "%2C" + coords[3] + "&user_location=" + coords[0] + "%2C" + coords[1] + "&gender=" + coords[4] + "&sort=distance-asc&skip=0&limit=20&user_key=c0c37ba24cca638d819d6ee47807bc99";
+    var requestUrl = "https://api.betterdoctor.com/2016-03-01/doctors?query=" + coords[2] + "&location=" + coords[0] + "%2C" + coords[1] + "%2C" + coords[3] + "&user_location=" + coords[0] + "%2C" + coords[1] + "&gender=" + coords[4] + "&sort=distance-asc&skip=0&limit=20&user_key=" + key;
     console.log(requestUrl);
     $.get(requestUrl)
         .then(function(result) {
+            $("#results").html("");
             for (i = 0; i < result.data.length; i++) {
-                $("#results").append("<div class='card blue-grey'><div class='card-image'><img src=" + result.data[i].profile.image_url + "></div><div class='card-content white-text'><span class='card-title'>" + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + " " + result.data[i].profile.title + "</span><p>" + result.data[i].profile.bio + "</p></div><div class='card-action'><a href=" + result.data[i].practices[0].website + ">Website</a></div></div>")
+                $("#results").append("<div class='card blue-grey'><div class='card-image'><img src=" + result.data[i].profile.image_url + "></div><div class='card-content white-text'><span class='card-title'>" + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + " " + result.data[i].profile.title + "</span><p>" + result.data[i].profile.bio.substring(0, 50) + "..." +  "</p></div><div class='card-action'><a href=" + result.data[i].practices[0].website + ">Website</a></div></div>")
             }
         })
         .fail(function(error) {
